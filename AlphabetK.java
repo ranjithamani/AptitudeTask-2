9ee6eb53a0c020ca633671a0d24cc989aa062ebd
ranjithamani <83059639+ranjithamani@users.noreply.github.com>
2021-08-04 16:48:24 +0530

    Add files via upload

--- AlphabetK.java
+++ AlphabetK.java
@@ -1,20 +1,17 @@
 import java.util.*;
 class AlphabetK{
 public static void main(String[] args){
-
 int i,j,k=8;
 int n=0;
 int m=5;
-
 for(i=0;i<=k;i++){
+	
 for(j=0;j<=5;j++){
-
 if(j==0 ||(i==j+3 && j>1))
-System.out.print("**");
-
-else if(
-i==n && j==m){
-System.out.print("**");
+	
+System.out.print("*");
+else if(i==n && j==m){
+System.out.print("*");
 n=n+1;
 m=m-1;
 	
@@ -26,5 +23,3 @@ System.out.println();
 }
 }
 }
-
-
